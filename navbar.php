48494a74c527373b36d4dc92236337397d5dfc7e
Aryan Vidyarthi <68266544+Aryanvdy@users.noreply.github.com>
2021-07-20 23:16:05 +0530

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,34 @@
+<!-- navbar --> 
+
+<section class="nav_sect">
+<nav class="navbar navbar-expand-md">
+      <a class="navbar-brand" href="index.php">
+        <img src="img/Hiii.png" width="175px" class="saflogo">
+      </a>
+      <button class="navbar-toggler ml-auto custom-toggler" 
+                type="button" 
+                data-toggle="collapse" 
+                data-target="#collapsibleNavbar">
+                <div class="ham_bt"></div>
+                <div class="ham_bt"></div>
+                <div class="ham_bt"></div>
+        </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ms-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php"><b>Home</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="createuser.php"><b>Create User</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php"><b>Transfer Money</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php"><b>Transaction History</b></a>
+              </li>
+          </div>
+         
+       </nav>
+
+</section>
